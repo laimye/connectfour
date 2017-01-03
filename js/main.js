0a17425765e24ef73f6b482249ba7df251e8bfb3
Elena <elena.korotich@gmail.com>
2017-01-02 17:11:19 -0800

    first commit

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ a.		Update display
 */
 
 /* --- Application-Wide variables ----*/
+
 var board = [
 	['', '', '', '', '', ''],
 	['', '', '', '', '', ''],
@@ -29,38 +30,37 @@ var board = [
 	['', '', '', '', '', '']
 ];
 
-var boardIdx;
-
+var boardIdx, winnerFound, cell, clickedEl, curColumn;
 var lastElement = [6, 6, 6, 6, 6, 6];
-
 var player = 'red';
-var cell, clickedEl;
-var curColumn;
+
 
 /* ---- DOM Element Variables ---- */
 
 var $msg = $('#msg');
 
+
 /* ---- Event Listeners ---- */
 $('.arrow').on('click', handleClick);
 
+$('#reset-button').on('click', initialize);
 
 
 /* ---- Functions ---- */
 
 function initialize () {
-	$msg.html('Welcome to Connect Four!');
+	$msg.html('Welcome to Gravitrips!');
 	player = 'black';
+	$('td').removeClass('red').removeClass('black');
+	winnerFound = false;
+	document.getElementsByClassName('arrow').disabled = false;
+	lastElement = [6, 6, 6, 6, 6, 6];
 	board = [['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', ''], ['', '', '', '', '', '']];
-	//$('.cell').removeClass('red').removeClass('black');
 }
-
 initialize();
-
 
 function handleClick(evt){
 	var clickedEl = evt.target;
-	console.log(evt.target.id);
 	if (evt.target.id == 'c1') {
 		curColumn = 0;
 	} else if (evt.target.id == 'c2') {
@@ -74,114 +74,160 @@ function handleClick(evt){
 	} else if (evt.target.id == 'c6') {
 		curColumn = 5;
 	};
-	console.log(curColumn);
 	checkRow();
-	
-	// checkIfOccupied();
-	// handleMove();
-	// renderBoard();
-	// checkWinner();
-	// checkIfTie();
-	// switchPlayer();
+	checkWinner(curColumn, lastElement[curColumn]);
 }
 
 function checkRow () {
 	lastElement[curColumn] = lastElement[curColumn] - 1;
-	console.log(lastElement[curColumn]);
 	if (lastElement[curColumn] < 0) alert('This column is full. Please try again.');
 	else {
 		board[curColumn][lastElement[curColumn]] = player;
 		boardIdx = 10*lastElement[curColumn] + curColumn;
 		switchPlayer();
 	}
-	console.log(board);
 	$('#' + parseFloat(boardIdx)).addClass(player);
-	// renderBoard();
 };
 
-
-// function renderBoard () {
-// 	var idx = lastElement[curColumn];
-// 	var cell = board[curColumn][idx];
-// 	cell.textContent = 'red';
-// 	//cell.evt.target.id = 'red';
-// 	//board[curColumn][idx].innerHTML('player');
-// 	//board[30]='X';
-// 	console.log(board);
-// };
-
-// checkIfOccupied(col, row) {
-// 	var occupied = board[row][col];
-// 	return value === null ? false ; true;
-// };
-
-
-
-// add circle to the board by adding class
-//the circle drops to the very bottom
-// handleMove() {
-// 	$('.cell').addClass(player);
-// };
-
-
-// renderBoard() {
-
-// };
-
-// checkWinner() {
-// 	//vertical
-// 	if ((board[curColumn][2] == board[curColumn][3] && board[curColumn][2] != '' && (board[curColumn][2] == board[curColumn][1] && board[curColumn][1] == board[curColumn][0]) || (board[curColumn][3] == board[curColumn][4] && board[curColumn][4] == board[curColumn][5])
-// 		//horizontal
-// 		|| (board[2][] == board[3][idx] && board[3][idx] != '' && (board[2][idx] == board[1][idx] && board[1][idx] == board[0][idx]) || (board[3][idx] == board[4][idx] && board[4][idx] == board[5][idx])) {
-// 			return true;
-// 			} else if (idx >= 3 && (board[curColumn][idx] == board[curColumn + 1][idx - 1] && board[curColumn + 1][idx - 1] == board[curColumn + 2][idx - 2] && board[curColumn + 3][idx - 3] == board[curColumn + 2][idx - 2]) {
-
-// 			}
-// 		)
-// };
-
-function checkWinner() {
+// 1. checkWinner - function is triggered after each move.
+// 2. the program will loop through the active row/column/diagonal looking for the cell of the same color next to each other// 	a. 
+//	a. determine where the active row/column/diagonal starts
+//	b. define the starting point of the row/column/diagonal that we check as currentPlayer
+//	c. compare each cell in the active row/column/diagonal starting from the starting point to the following cell
+//		1. if the cell mathes the previous and != '', add 1 count;
+//		2. if the cell does not match, count = 1;
+// 	d. if the count = 4, the latest player wins.
+function checkWinner(col1, row1) {
 	count = 1;
-	board[x][y]
-	player = board[0][y];
-	for (x = 1; x <= 5; x++) {
-		if (board[x][y] == player) {
+	var activeColumn, activeRow;
+	var curPlayer = board[0][row1];
+	for (activeColumn = 1; activeColumn <= 5; activeColumn++) {
+		if (board[activeColumn][row1] == curPlayer && curPlayer != '') {
 			count += 1;
 		} else {
 			count = 1;
 		}
-	}
-	if (count == 4) {
-		return 'Congratulations, player ' + player + ' won!'
+		if (count == 4) winnerFound = true;
+		curPlayer = board[activeColumn][row1];		
 	}
 
+	// check vertical
+	if (winnerFound == false) {
+		curPlayer = board[col1][0];
+		for (activeRow = 1; activeRow <= 5; activeRow ++) {
+			if (board[col1][activeRow] == curPlayer && curPlayer != '') {
+				count += 1;
+			} else {
+				count = 1;
+			}
+			if (count == 4) winnerFound = true;
+			curPlayer = board[col1][activeRow];
+		}
+	}
+
+	// //check diagonal - bottom-up to the right
+	if (winnerFound == false) {
+		activeColumn = col1;
+		activeRow = row1;
+		count = 1;
+		var startingCol, startingRow;
+		var stepsToCol = col1;
+		var stepsToRow = 5 - row1;
+		if (stepsToCol > stepsToRow) { // starting from bottom
+			startingCol = col1 - stepsToRow;
+			startingRow = 5; 
+			curPlayer = board[startingCol][startingRow];
+			for (activeColumn = startingCol + 1, activeRow = startingRow - 1; activeRow >= 0 && activeColumn <= 5; activeRow --, activeColumn++) {
+				if (board[activeColumn][activeRow] == curPlayer && curPlayer != '') {
+				count += 1;
+				} else {
+					count = 1;
+				}
+				curPlayer = board[activeColumn][activeRow];
+				if (count == 4) winnerFound = true;
+			}
+		} else { // starting from left side
+			startingCol = 0;
+			startingRow = col1 + row1;
+			curPlayer = board[startingCol][startingRow];
+			for (activeColumn = startingCol + 1, activeRow = startingRow - 1; activeRow >= 0 && activeColumn <= 5; activeRow --, activeColumn++) {
+				if (board[activeColumn][activeRow] == curPlayer && curPlayer != '') {
+				count += 1;
+				} else {
+					count = 1;
+				}
+				if (count == 4) winnerFound = true;
+				curPlayer = board[activeColumn][activeRow];
+			}
+		}
+	}
+
+	// check diagonal - bottom-up to the left
+	if (winnerFound == false) {
+		activeColumn = col1;
+		activeRow = row1;
+		count = 1;
+		stepsToCol = 5 - col1;
+		stepsToRow = 5 - row1;
+		if (stepsToCol > stepsToRow) { //starting from bottom
+			startingCol = col1 + stepsToRow;
+			startingRow = 5;
+			curPlayer = board[startingCol][startingRow];
+			for (activeColumn = startingCol - 1, activeRow = startingRow - 1; activeRow >= 0 && activeColumn >= 0; activeRow --, activeColumn --) {
+				if (board[activeColumn][activeRow] == curPlayer && curPlayer != '') {
+				count += 1;
+				} else {
+					count = 1;
+				}
+				if (count == 4) winnerFound = true;
+				curPlayer = board[activeColumn][activeRow];
+			}
+		} else { // starting from right side
+			startingCol = 5;
+			startingRow = row1 + stepsToCol;
+			curPlayer = board[startingCol][startingRow];
+			for (activeColumn = startingCol - 1, activeRow = startingRow - 1; activeRow >= 0 && activeColumn >= 0; activeRow --, activeColumn --) {
+				if (board[activeColumn][activeRow] == curPlayer && curPlayer != '') {
+				count += 1;
+				} else {
+					count = 1;
+				}
+				if (count == 4) winnerFound = true;
+				curPlayer = board[activeColumn][activeRow];
+			}
+		}
+	}
+
+	if (winnerFound) {
+		$msg.html('Congratulations, player ' + player + ' won!');
+		winnerFound = true;
+		document.getElementsByClassName('arrow').disabled = true;
+	}
+	return winnerFound;
+	// checkIfTie();
 }
 
-//tie game
+//check if the game is tie
 // function checkIfTie () {
-// 	for (var col = 0; col <= 6, col ++) {
-// 		for (var row = 0; row <= 6, row ++) {
-// 			if (board[row][col] === null) {
+// if (winnerFound == false) {
+// 	for (var col = 0; col <= 5, col ++) {
+// 		for (var row = 0; row <= 5, row ++) {
+// 			if (board[col][row] === '') {
 // 				return false;
 // 			} else {
-// 				return true;
+// 				$msg.html("It's a tie!");
 // 			}
 // 		}
 // 	}
 // }
 
+
+//switch player and display whose turn it is. 
 function switchPlayer() {
-	// player = (player === player1)? player2 : player1;
 	player = (player === 'red')? 'black' : 'red';
+	if (player == 'red') $msg.html('Turn: Player Black');
+	else $msg.html('Turn: Player Red');
 	return player;
 };
-
-
-// Trigger the game sequence by clicking on a position button on the board
-// $('.board button').click(function(e) {
-//     // Detect the x and y position of the button clicked.
-//     var y_pos = $('.board tr').index($(this).closest('tr'));
-//     var x_pos = $(this).closest('tr').find('td').index($(this).closest('td'));
-// });
 
 }); // end of jQuery function
